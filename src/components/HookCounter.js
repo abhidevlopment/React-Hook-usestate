03179a50128b60528363cba0583520e4ae158a52
abhidevlopment <abhidevlopment4@gmail.com>
2019-08-16 21:48:43 +0530

    commited by abhi

---
+++ src/components/HookCounter.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function HookCounter() {
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+  return (
+    <div>
+      Count:{count} &nbsp;
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+export default HookCounter;
